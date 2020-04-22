9271618db8a7776e9f575775d81d4db9909d662e
AmirSejdinovic <37899933+AmirSejdinovic@users.noreply.github.com>
2020-04-22 12:40:35 +0200

    Adding loader and fetch another set of posts from api

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const postContainer = document.getElementById("post-container");
 const loading = document.querySelector(".loader");
 const filter = document.getElementById("filter");
 
-let limit = 3;
+let limit = 5;
 let page = 1;
 
 //Fetch posts from api
@@ -34,5 +34,27 @@ async function showPosts() {
   });
 }
 
+//Show loader & fetch more posts
+function showLoading() {
+  loading.classList.add("show");
+
+  setTimeout(() => {
+    loading.classList.remove("show");
+
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 3000);
+  }, 2000);
+}
 //show inital post
 showPosts();
+
+//Scroll
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
